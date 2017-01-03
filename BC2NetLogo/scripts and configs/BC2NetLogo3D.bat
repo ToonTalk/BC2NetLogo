cf7b7c2757813191f40f948cab4f7842768bc256
Ken Kahn <toontalk@gmail.com>
2017-01-03 12:35:35 +0800

    Added 3D BC2NetLogo scripts

---
+++ BC2NetLogo/scripts and configs/BC2NetLogo3D.bat
@@ -0,0 +1,1 @@
+java -Dorg.nlogo.is3d=true -Xfuture -Xmx1024m -Dfile.encoding=UTF-8 -jar BC2NetLogo.jar
---
+++ BC2NetLogo/scripts and configs/BC2NetLogo3D.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -Dorg.nlogo.is3d=true -Xfuture -Xmx1024m -Dfile.encoding=UTF-8 -jar BC2NetLogo.jar
